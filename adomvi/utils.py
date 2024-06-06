ddf98e5de19311a86900ad842853344efd918810
Jonas Renault <jonasrenault@gmail.com>
2024-06-06 14:15:20 +0200

    Cleanup notebook 01

---
+++ adomvi/utils.py
@@ -0,0 +1,24 @@
+import logging
+import requests
+import shutil
+from pathlib import Path
+
+LOG = logging.getLogger()
+
+
+def download_file(url: str, filename: Path):
+    """
+    Download a file from url to filename.
+
+    Args:
+        url (str): the url to download
+        filename (Path): the destination file on disk
+    """
+    if filename.exists():
+        LOG.info(f"File {filename} already exists. Skipping download.")
+    else:
+        LOG.info(f"Downloading {filename} from {url} ...")
+        with requests.get(url, stream=True) as r:
+            with open(filename, "wb") as f:
+                shutil.copyfileobj(r.raw, f)
+        LOG.info("Download complete.")
