9a21d6eb7cff538f256d23e04abd0775c7c4c631
Ulrich Kemka Takengny <ukemkata@PC-01002886.siege.inria.fr>
2024-07-11 18:31:14 +0200

    Add augment dataset function

--- adomvi/datasets/augmentation.py
+++ adomvi/datasets/augmentation.py
@@ -111,7 +111,8 @@ def apply_augmentation(image_path: Path, label_path: Path, augmentation_func):
     aug_bboxes = voc_to_yolo(image.shape, aug_bboxes)
 
     # Save augmented image
-    aug_image_path = str(image_path).replace(".jpg", f"_augmented.jpg")
+    ext = image_path.suffix
+    aug_image_path = str(image_path).replace(ext, f"_augmented{ext}")
     cv2.imwrite(aug_image_path, aug_image)
 
     # Save augmented labels
@@ -155,10 +156,10 @@ def func_augmentation(image: np.ndarray, bboxes: list):
     transform = A.Compose(
         [
             A.Affine(
-                scale=(0.1, 0.9),  # ScaleFloatType | dict[str, Any] | None
-                translate_percent=(-0.3, 0.3),  # ScaleFloatType | dict[str, Any] | None
-                rotate=None,  # ScaleFloatType | None
-                p=1.0,  # float
+                scale=(0.1, 0.9),  
+                translate_percent=(-0.3, 0.3),
+                rotate=None,
+                p=1.0,
             ),
             A.CoarseDropout(
                 max_holes=10,
@@ -180,3 +181,24 @@ def func_augmentation(image: np.ndarray, bboxes: list):
         image=image, bboxes=bboxes, class_labels=["label"] * len(bboxes)
     )
     return augmented["image"], augmented["bboxes"]
+
+
+def augment_dataset(dataset_path: Path, augmentation_func):
+    """
+    Augment the dataset by applying the augmentation function to each image and its corresponding labels.
+
+    Args:
+        dataset_path (Path): Path to the dataset.
+        augmentation_func (callable): Augmentation function to apply to each image and its labels.
+    """
+
+    split_dirs = ["train", "test"]
+    for split_name in split_dirs:
+        image_path = dataset_path / f"images/{split_name}"
+        label_path = dataset_path / f"labels/{split_name}"
+
+        image_paths = list(image_path.glob("*"))
+        label_paths = list(label_path.glob("*"))
+
+        for image_path, label_path in zip(image_paths, label_paths):
+            apply_augmentation(image_path, label_path, augmentation_func)
